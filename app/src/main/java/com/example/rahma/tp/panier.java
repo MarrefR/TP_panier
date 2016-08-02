4f8d056b0d807b6004ed33b3be6118f6c410f0ba
rahma <fsd>
2016-08-02 19:06:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rahma/tp/panier.java
@@ -0,0 +1,13 @@
+package com.example.rahma.tp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class panier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panier);
+    }
+}
